730b8b894d34fa0f11442534f59108b4b9dd36d9
Shindo Hikaru <microapple56@gmail.com>
2016-03-07 17:19:22 +0900

    add-delete-csv-button

--- main.py
+++ main.py
@@ -148,6 +148,11 @@ def download(filename):
     """donwload file"""
     return send_from_directory('.', filename)
 
+@app.route("/delete", methods = ['POST'])
+def deleteAllCSV():
+    os.system("rm csv/*")
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run(threaded=True)
